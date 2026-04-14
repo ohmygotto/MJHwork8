6827cf163f70344d9df852f20011d4bf8f8df77a
MIN <nakbkqwe21@gmail.com>
2026-04-14 17:33:02 +0900

    unreal 파일 추가및 4챕터 완료 버전

---
+++ Source/SpartaProject/Public/MineItem.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseItem.h"
+#include "MineItem.generated.h"
+
+UCLASS()
+class SPARTAPROJECT_API AMineItem : public ABaseItem
+{
+	GENERATED_BODY()
+
+public:
+	AMineItem();
+
+	USphereComponent* ExplosionCollision;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Effects")
+	UParticleSystem* ExplosionParticle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Effects")
+	USoundBase* ExplosionSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mine")
+	float ExplosionDelay;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mine")
+	float ExplosionRadius;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mine")
+	int32 ExplosionDamage;
+
+	bool bHasExploded;
+
+	FTimerHandle ExplosionTimerHandle;
+
+	virtual void ActivateItem(AActor* Activator) override;
+
+	void Explode();
+};
